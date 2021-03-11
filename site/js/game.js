b4842dba6e24c8a5bda02293b6e0ec35a19e4fa9
SushyDev <xsushy@hotmail.com>
2021-03-11 20:58:58 +0100

    UI Overhaul, using grid system now

--- site/js/game.js
+++ site/js/game.js
@@ -4,62 +4,64 @@ import dices from '../dice/*.svg';
 let stats = {
     correct: 0,
     incorrect: 0,
-    rolls: -1,
+    rolls: 0,
 };
 
-const hardMode = () => (sessionStorage.getItem('penguings') == 'true' ? true : false);
+const hardMode = () => (sessionStorage.getItem('penguings') === 'true' ? true : false);
 
 // ! Build answer based on current dice
 const getAnswer = () => {
-    const correct = hardMode()
+    const answer = hardMode()
         ? {
               icebears: 0,
-              wakken: 0,
+              iceholes: 0,
               penguings: 0,
           }
         : {
               icebears: 0,
-              wakken: 0,
+              iceholes: 0,
           };
 
-    console.log(correct);
-
-    // ? For each dice in container check values
+    // # For each dice in container check values
     document.getElementById('dices').childNodes.forEach((dice) => {
         const number = dice.value;
 
-        if (number === 6 && hardMode()) correct.penguings += 3;
-        if (number === 5) {
-            correct.icebears += 4;
-            correct.wakken++;
-            if (hardMode()) correct.penguings += 4;
-        }
-        if (number === 4 && hardMode()) correct.penguings += 5;
-        if (number === 3) {
-            correct.icebears += 2;
-            correct.wakken++;
-            if (hardMode()) correct.penguings += 6;
-        }
-        if (number === 2 && hardMode()) correct.penguings += 1;
-        if (number === 1) {
-            correct.icebears += 0;
-            correct.wakken++;
-            if (hardMode()) correct.penguings += 2;
+        if (hardMode()) {
+            if (number === 6) answer.penguings += 3;
+            if (number === 5) answer.penguings += 4;
+            if (number === 4) answer.penguings += 5;
+            if (number === 3) answer.penguings += 6;
+            if (number === 2) answer.penguings += 1;
+            if (number === 1) answer.penguings += 2;
         }
 
-        // ! Write algorithm to calculate bottom side of dice
+        if (number === 5) {
+            answer.icebears += 4;
+            answer.iceholes++;
+        }
+        if (number === 3) {
+            answer.icebears += 2;
+            answer.iceholes++;
+        }
+        if (number === 1) {
+            answer.icebears += 0;
+            answer.iceholes++;
+        }
+
+        // ! Write algorthemithm to calculate bottom side of dice
         /*
-        1 = 2 / 2 = 1
-        3 = 6 / 6 = 3
-        4 = 5 / 5 = 4
+        ? 1 = 2 / 2 = 1
+        ? 3 = 6 / 6 = 3
+        ? 4 = 5 / 5 = 4
         */
     });
 
-    return correct;
+    return answer;
 };
 
 // ! Create a dice with a random number between 1-6
 const createDice = () => {
+    // ? Roll random number between 1-6
     const rand = Math.floor(Math.random() * 6) + 1;
 
     const dice = document.createElement('div');
@@ -70,6 +72,7 @@ const createDice = () => {
     img.src = dices[rand];
 
     dice.appendChild(img);
+
     return dice;
 };
 
@@ -78,42 +81,49 @@ const createDiceOption = (num) => {
     const option = document.createElement('option');
     option.value = num;
     option.textContent = num;
+
     return option;
 };
 
-// ! Add options to select
+// # Add options 3-12 to select
 for (let added = 3; added <= 12; added++) document.getElementById('dice-count').appendChild(createDiceOption(added));
 
 // ? Winows functions
+
 // ! Log answer
 window.get = () => console.log(getAnswer());
 
 // ! Verify answer
 window.answer = function answer() {
     const icebears = parseInt(document.getElementById('icebears').value);
-    const wakken = parseInt(document.getElementById('wakken').value);
+    const iceholes = parseInt(document.getElementById('iceholes').value);
     const penguings = parseInt(document.getElementById('penguings').value);
-    const answer = hardMode() ? {icebears, wakken, penguings} : {icebears, wakken};
+    const answer = hardMode() ? {icebears, iceholes, penguings} : {icebears, iceholes};
 
-    console.log(getAnswer(), answer);
-
-    // ?  On correct
+    // ? On correct
     if (JSON.stringify(answer) === JSON.stringify(getAnswer())) {
         alert('Congratiolations, thats the correct answer');
         stats.correct++;
-        document.getElementById('stats-correct').textContent = `Answered correctly: ${stats.correct}`;
+        document.getElementById('stats-correct').value = stats.correct;
     } else {
         alert('You guessed it wrong, retry');
         stats.incorrect++;
-        document.getElementById('stats-incorrect').textContent = `Answered wrong: ${stats.incorrect}`;
+        document.getElementById('stats-incorrect').value = stats.incorrect;
     }
 
-    // ? Display correct answer
-    document.getElementById('icebears-correct').value = getAnswer().icebears;
-    document.getElementById('wakken-correct').value = getAnswer().wakken;
-    document.getElementById('penguings-correct').value = getAnswer().penguings;
+    // # Display correct answer
+    document.getElementById('icebears-correct').value = getAnswer().icebears || 0;
+    document.getElementById('iceholes-correct').value = getAnswer().iceholes || 0;
+    document.getElementById('penguings-correct').value = getAnswer().penguings || 0;
 
-    // ? Reset
+    // # Reset
+    addDices(document.getElementById('dice-count').value);
+};
+
+// ! Reroll dice
+window.reroll = function reroll() {
+    stats.rolls++;
+    document.getElementById('stats-rolls').value = stats.rolls;
     addDices(document.getElementById('dice-count').value);
 };
 
@@ -121,11 +131,11 @@ window.answer = function answer() {
 window.addDices = function addDices(amount) {
     const container = document.getElementById('dices');
     container.innerHTML = '';
-    stats.rolls++;
-    document.getElementById('stats-rolls').textContent = `Rolled the dice: ${stats.rolls}`;
+
     for (let added = 1; added <= amount; added++) container.appendChild(createDice());
 };
-addDices(3);
 
+// # Do some stuff on load
+addDices(3);
 window.toggleMode = (elem) => sessionStorage.setItem('penguings', elem.checked);
 sessionStorage.setItem('penguings', false);
--- site/js/main.js
+++ site/js/main.js
@@ -1,3 +1,9 @@
-const {lockSize} = require('../js/screen-size');
+// ! Chrome mobile viewport height hack
+function lockSize(query) {
+    const elem = document.querySelector(query);
+    const vh = window.innerHeight * 0.01;
+    elem.style.setProperty('--vh', `${vh}px`);
+}
+
 lockSize('body');
 window.addEventListener('resize', () => lockSize('body'));
